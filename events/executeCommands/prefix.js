8b36d71cfe204721ddf8ffa42e2449711bfd2477
arthur <arthur.puechberty@outlook.com>
2024-06-03 22:38:35 +0200

    update

--- events/executeCommands/prefix.js
+++ events/executeCommands/prefix.js
@@ -12,12 +12,15 @@ module.exports = {
     const command = client.commands.get(commandName) || client.commands.find(cmd => cmd.aliases && cmd.aliases.includes(commandName));
     if (!command) return;
 
-    if (command.dm !== true && message.channel.type === 1) return;
-    if (command.botOwnerOnly && !client.config.owner.includes(message.author.id)) return;
-    if (command.permissions && !message.member.permissions.has(command.permissions) && !client.config.owner.includes(message.author.id)) return;
+    if (command.dm !== true && message.channel.type === 1) return message.reply({ content: 'Cette commande ne peut pas être utilisée en message privé.' }).then(msg => setTimeout(() => msg.delete(), 5000));
+    if (client.config.owners && !client.config.owners.includes(message.author.id)) {
+      if (command.botOwnerOnly) return message.reply({ content: 'Cette commande est réservée au propriétaire du bot.' }).then(msg => setTimeout(() => msg.delete(), 5000));
+      if (command.permissions && message.channel.type !== 1 && !command.permissions.every(permission => message.member.permissions.has(permission))) return message.reply({ content: 'Vous n\'avez pas la permission d\'utiliser cette commande.' }).then(msg => setTimeout(() => msg.delete(), 5000));
+    }
 
     try {
       command.executePrefix(client, message, args);
+      console.log(`[CMD - PREFIX] ${message.author.tag} | ${commandName} | [${message.guild.name} - ${message.channel.name}]`);
     } catch (error) {
       console.error(`Erreur lors de l'exécution de la commande '${commandName}':`, error);
     }
--- events/executeCommands/slash.js
+++ events/executeCommands/slash.js
@@ -6,18 +6,15 @@ module.exports = {
     const command = client.commands.get(interaction.commandName);
     if (!command) return;
 
-    if (command.dm !== true && interaction.channel.type === 1) {
-      return interaction.reply({ content: 'Cette commande ne peut pas être utilisée en message privé.', ephemeral: true });
-    }
-    if (command.botOwnerOnly && !client.config.owner.includes(interaction.user.id)) {
-      return interaction.reply({ content: 'Cette commande est réservée au propriétaire du bot.', ephemeral: true });
-    }
-    if (command.permissions && !interaction.member.permissions.has(command.permissions) && !client.config.owner.includes(message.author.id)) {
-      return interaction.reply({ content: 'Vous n\'avez pas la permission d\'utiliser cette commande.', ephemeral: true });
+    if (command.dm !== true && interaction.channel.type === 1) return interaction.reply({ content: 'Cette commande ne peut pas être utilisée en message privé.', ephemeral: true });
+    if (client.config.owners && !client.config.owners.includes(interaction.user.id)) {
+      if (command.botOwnerOnly) return interaction.reply({ content: 'Cette commande est réservée au propriétaire du bot.', ephemeral: true });
+      if (command.permissions && interaction.channel.type !== 1 && !command.permissions.every(permission => interaction.member.permissions.has(permission))) return interaction.reply({ content: 'Vous n\'avez pas la permission d\'utiliser cette commande.', ephemeral: true });   
     }
 
     try {
       command.executeSlash(client, interaction);
+      console.log(`[CMD - SLASH] ${interaction.user.tag} | ${interaction.commandName} | [${interaction.guild.name} - ${interaction.channel.name}]`);
     } catch (error) {
       console.error(`Erreur lors de l'exécution de la commande slash '${interaction.commandName}':`, error);
     }
